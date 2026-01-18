7dd323b61a08cb60dabf9aa7811b09b8876e8fee
WallOfNoise <z5338374@ad.unsw.edu.au>
2026-01-19 03:42:27 +1100

    Created full directory for Streamlit app to ensure deployability later

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,1 +1,26 @@
-print("This is where the dashboard will be implemented!")
+import streamlit as st
+import pandas as pd
+from numpy.random import default_rng as rng
+
+
+st.title("Hello World!")
+
+with st.sidebar:
+    st.header("About App")
+    st.write("Hey! You found the sidebar!")
+
+st.header("_Streamlit_ is :blue[cool] :sunglasses:")
+st.header("This is a header with a divider", divider="gray")
+
+st.markdown("This is created using st.markdown")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    x = st.slider("Choose an x value", 1, 10)
+with col2:
+    st.write("The value of :red[x] is", x)
+
+df = pd.DataFrame(rng(0).standard_normal((20, 3)), columns=["a", "b", "c"])
+
+st.bar_chart(df, horizontal=True)
