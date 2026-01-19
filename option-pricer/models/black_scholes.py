e3f4c5f2e59c480caa11e216a5acc9463715a63d
WallOfNoise <z5338374@ad.unsw.edu.au>
2026-01-19 13:28:13 +1100

    Refactored black_scoles.py as a decoupled function that is called by front end to determine call/put option price

--- option-pricer/models/black_scholes.py
+++ option-pricer/models/black_scholes.py
@@ -1,28 +1,33 @@
 import math
 from scipy.stats import norm
 
-# UPDATE TO INTAKE FROM USER INPUT !!!
+def black_scholes_price(
+        S :float, # Underlying Price
+        K :float,  # Strike Price
+        T :float, # Time to Expiration (6 mths)
+        r :float, # Risk-Free Rate (yield of US 10 year treasury bond)
+        vol :float, # volatility (σ), 
+        option_type:str # either call or put
 
-S = 42 # Underlying Price
-K = 40 # Strike Price
-T = 0.5 # Time to Expiration (6 mths)
-r = 0.1 # Risk-Free Rate (yield of US 10 year treasury bond)
-vol = 0.2 # volatility (σ)
+) -> dict:
+    if S <= 0 or K <= 0 or T <= 0 or vol <= 0:
+        raise ValueError("Invalid input: S, K, T and volatility must be positive")
+    # Calculate d1
+    d1 = (math.log(S/K) + (r + 0.5*(vol **2)) * T) / (vol * (math.sqrt(T))) 
+    # Calculate d2
+    d2 = d1 - (vol * (math.sqrt(T)))
 
-# Calculate d1
-d1 = (math.log(S/K) + (r + 0.5*(vol **2)) * T) / (vol * (math.sqrt(T))) 
-# Calculate d2
-d2 = d1 - (vol * (math.sqrt(T)))
+    if option_type.lower() == "call":
+        # Calculate Call Option Price
+        price = (S * norm.cdf(d1)) - (K * math.exp(-r*T) * norm.cdf(d2))
+    elif option_type.lower() == "put":
+        # Calculate Put Option Price
+        price = (K * math.exp(-r*T) * norm.cdf(-d2)) - (S * norm.cdf(-d1))
+    else:
+        raise ValueError("Option type must be 'call' or 'put'")
 
-# Calculate Call Option Price
-C = (S * norm.cdf(d1)) - (K * math.exp(-r*T) * norm.cdf(d2))
-# Calculate Put Option Price
-P = (K * math.exp(-r*T) * norm.cdf(-d2)) - (S * norm.cdf(-d1))
-
-# CHANGE RETURN TO PROVIDE VALUE
-
-# Printing the results (d1/2 rounded to 4dp, C and P rounded to 2dp)
-print(f'The value of d1 is: {round(d1, 4)}')
-print(f'The value of d2 is: {round(d2, 4)}')
-print(f'The price of the call option is: ${round(C, 2)}')
-print(f'The price of the put option is: ${round(P, 2)}')
+    return {
+        "price": price,
+        "d1": d1,
+        "d2": d2
+    }
