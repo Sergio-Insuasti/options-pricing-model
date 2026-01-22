c879b1f4c0ac796b4c10f3662c76f5edce51efd9
WallOfNoise <z5338374@ad.unsw.edu.au>
2026-01-22 15:16:22 +1100

    Refactored tab creation to layout.py . Now all functionality is separated from streamlit_app.py . Blog post incoming

--- app/layout.py
+++ app/layout.py
@@ -1,5 +1,9 @@
 import streamlit as st
-
+from app.tabs.overview import render_overview
+from app.tabs.comparison import render_comparison
+from app.tabs.convergence import render_convergence
+from app.tabs.sensitivity import render_sensitivity
+from app.tabs.diagnostics import render_diagnostics
 # ===============================
 # Page Config
 # ===============================
@@ -21,3 +25,27 @@ def render_layout():
     st.title("Option Pricing Models Dashboard")
     LI_url = "https://www.linkedin.com/in/sergio-insuasti/"
     st.subheader(f"By [Sergio Insuasti]({LI_url})")
+    
+def render_tabs():
+    tab1, tab2, tab3, tab4, tab5 = st.tabs([
+        "Overview",
+        "Model Comparison",
+        "Convergence",
+        "Sensitivity (Greeks)",
+        "MC Diagnostics"
+    ])
+    with tab1:
+        render_overview()
+
+    with tab2:
+        render_comparison()
+
+    with tab3:
+        render_convergence()
+
+    with tab4:
+        render_sensitivity()
+
+    with tab5:
+        render_diagnostics()
+    
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,12 @@
 import streamlit as st
 
 from app.state import initialise
-from app.layout import render_layout
+from app.layout import render_layout, render_tabs
 from app.sidebar import render_sidebar
-from app.tabs.overview import render_overview
-from app.tabs.comparison import render_comparison
-from app.tabs.convergence import render_convergence
-from app.tabs.sensitivity import render_sensitivity
-from app.tabs.diagnostics import render_diagnostics
+
 
 initialise()
 render_layout()
 render_sidebar()
+render_tabs()
 
-tab1, tab2, tab3, tab4, tab5 = st.tabs([
-    "Overview",
-    "Model Comparison",
-    "Convergence",
-    "Sensitivity (Greeks)",
-    "MC Diagnostics"
-])
-
-with tab1:
-    render_overview()
-
-with tab2:
-    render_comparison()
-
-with tab3:
-    render_convergence()
-
-with tab4:
-    render_sensitivity()
-
-with tab5:
-    render_diagnostics()
-    
